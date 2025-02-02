bcb2f1e0a2de2e23b11c4f5f7edb5ae0ab49f9d9
gfires <100485480+gfires@users.noreply.github.com>
2025-02-01 23:12:04 -0600

    Create neuro_model.py

---
+++ neuro_model.py
@@ -0,0 +1,11 @@
+# import packages
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+import xgboost as xgb
+import scipy.stats as stats
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report 
+from sklearn.metrics import accuracy_score
+import numpy as np
